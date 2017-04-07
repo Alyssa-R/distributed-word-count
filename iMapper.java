b83226e33b9f42353159ea4f6616598e9412b124
Alyssa-R <alyssa.w.rivera@gmail.com>
2017-04-07 16:33:42 -0400

    questions/outlines for iMapper and iMaster

--- iMapper.java
+++ iMapper.java
@@ -5,7 +5,18 @@ import java.rmi.RemoteException;
 import java.util.*;
 
 public interface iMapper extends Remote {
+  
+  /* begins a mapping task on the machine where it's called on?
+   * what is the name param? is probably useful for master to track mappers and know which ones have terminated?
+   * 
+   * 
+   */ 
   public iMapper createMapTask(String name) throws RemoteException, AlreadyBoundException;
+  
+  /* does actual word counting and saves the hashtable somewhere locally??
+   * lets master know that it has intermediate data when done?
+   * 
+   */ 
   public void processInput (String input, iMaster theMaster) throws RemoteException, AlreadyBoundException;
 
 
--- iMaster.java
+++ iMaster.java
@@ -7,8 +7,23 @@ import java.util.*;
 
 public interface iMaster extends Remote {
 
+  /* sends back customized array of reducers to each mapper, based off of the keys the mapper found
+   * step 5 (incl parts a and b) 
+   * 
+   */ 
   public iReducer[] getReducers(String[] keys) throws RemoteException, AlreadyBoundException;
+  
+  /*does not return anything
+   * should it take in a certain mapper to know which one to mark as done?
+   * happens to mapper after sending data to reducers
+   */ 
   public void markMapperDone() throws RemoteException;
-  public void receiveOutput (String kay, int value) throws RemoteException;
+  
+  /* 
+   * called by reducer onto master after reducer has finished work, to consildate final data
+   * stores in the final output file
+   * reducer process terminated immediately after this method called
+   */  
+  public void receiveOutput (String key, int value) throws RemoteException;
 
 }
