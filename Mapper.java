5510b3abf315c47972a495344febb1de7e0c2886
clarasmith <clara@smithsaltzman.com>
2017-05-06 16:39:43 -0400

    added main method to bind to rmi registry, cleaned up word processing in countWords

--- Mapper.java
+++ Mapper.java
@@ -1,3 +1,5 @@
+import java.rmi.registry.Registry;
+
 public class Mapper implements iMapper {
 
   private String name;
@@ -5,14 +7,15 @@ public class Mapper implements iMapper {
   
   //dummy constructor? idk I feel like createMapTask is the real constructor here
   //no actually createMapTask just exists to create a constructed object!!
-  public Mapper(String n){
+  public Mapper(String n) throws RemoteException() {
     name = n;
   }
   
   /* returns newly constructed mapper
    */ 
   public iMapper createMapTask(String name) throws RemoteException, AlreadyBoundException{
-    return Mapper(name);
+	  Mapper mapper = new Mapper(name);
+	  return mapper;
   }
   
   /* does actual word counting and saves the hashtable somewhere locally
@@ -22,16 +25,18 @@ public class Mapper implements iMapper {
   public void processInput (String input, iMaster theMaster) throws RemoteException, AlreadyBoundException {
     
     /*
-     * count the words
-     * make a hashtable --> array of keys to send to getReducers
-     * getReducers from master
+     * results = countWords on input
+     * get Keys from Hashtable, convert to String array
+     * call theMaster to get Reducers (via getReducers) from the key array
      * for each reducer/key:
      *     send corresponding value from hashtable reducer.input(value)
      * mapper mark as done
      */
-    
-    Hashtable<String, int> results = countWords(line);
-    String[] keys = results.getKeys(); //is this kosher/does it exist?
+
+
+    Hashtable<String, int> results = countWords(input);
+    Set<String> keysSet = results.keySet(); //get Keys as set
+    String [] keys = keySet.toArray(new String[keySet.size()]); //convert to String array
     iReducer[] reducers = theMaster.getReducers(keys);
     iReducer r = reducers[0];
     int val = 0;
@@ -47,10 +52,9 @@ public class Mapper implements iMapper {
   }
   
   
-  
   public Hashtable<String, int> countWords(String line){
     Hashtable<String, int> results = new Hashtable<String, int>();
-    String[] words = line.split(" ");
+    String[] words = line.toLowerCase().replaceAll("[^a-zA-Z\\s]", "").split("\\s");
     String w = "";
     
     for(int i=0; i<words.length; i++){
@@ -66,5 +70,29 @@ public class Mapper implements iMapper {
     return results;
   }
   
+  
+  /*
+   * Main taken almost directly from main method on Assignment 3
+   */
+  public static void main (String args[]) {
+	  try {
+		  // Initialize account info
+		  String selfPort = args[0];
+		  String selfIp = InetAddress.getLocalHost().getHostAddress();
+		  
+		  // Get the local registry
+		  Registry registry = LocateRegistry.getRegistry(selfIp, Integer.parseInt(selfPort));
+		  
+		  // Set up mapper
+		  Mapper mapper = new Mapper(); //OK to do?
+		  
+		  // Bind the remote object's stub in the registry
+		  registry.bind("Mapper", mapper);
+		  System.out.println("Mapper ready")
+	  } catch (Exception e) {
+		  System.err.println("Connection exception: " + e.toString());
+		  e.printStackTrace();
+	  }
+  }
 
 }
