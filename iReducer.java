a63481675fff0fc6b4327ee12304f8bf41dae2be
clarasmith <clara@smithsaltzman.com>
2017-05-08 15:14:15 -0400

    initial commit with all functions fleshed out

--- iReducer.java
+++ iReducer.java
@@ -5,7 +5,22 @@ import java.rmi.RemoteException;
 import java.util.*;
 
 public interface iReducer extends Remote {
-	public iReducer createReduceTalk(String key, iMaster master) throws RemoteException, AlreadyBoundException;
+	/*
+	 * begins reducer task when called by master on a given key
+	 * key is word
+	 * master is iMaster instance
+	**/
+	public iReducer createReduceTask(String key, iMaster master) throws RemoteException, AlreadyBoundException;
+
+	/*
+	 * called by Mapper on Reducer when done with count of specific word key
+	 * value is count of key by that mapper
+	**/
 	public void receiveValues(int value) throws RemoteException;
+
+	/*
+	 * Once Reducer has sent input to Master via the Master fxn receiveOutput,
+	 * terminate Reducer process
+	**/
 	public int terminate() throws RemoteException;
 }
